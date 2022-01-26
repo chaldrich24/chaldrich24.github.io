0cb5ddcfe05d0cad91d2ee8d83e93290d55f5249
chaldrich24 <chaldrich24@gmail.com>
2022-01-26 14:55:54 -0500

    add navbar and about section

---
+++ src/components/Nav/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Nav() {
+    return (
+        <header>
+            <h1>Chad Aldrich</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <span>About Me</span>
+                    </li>
+                    <li>
+                        <span>Projects</span>
+                    </li>
+                    <li>
+                        <span>Contact</span>
+                    </li>
+                    <li>
+                        <span>Resume</span>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Nav;
