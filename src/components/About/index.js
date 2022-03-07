715100fac0139853f5aab942ec110827e36a617f
chaldrich24 <chaldrich24@gmail.com>
2022-03-07 14:38:39 -0500

    update about section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,7 +7,8 @@ function About() {
             <p>
                 As seen in the web page title, my name is Chad Aldrich. I currently work as a financial analyst for
                 Leidos located in the DC metro area. I graduated from James Madison University in 2019 and, desiring
-                to progress into more complex subjects, I applied and joined this coding bootcamp for full-stack web
+                to progress into more complex subjects, I applied and joined George Washington University's
+                coding bootcamp for full-stack web
                 development. Ultimately, I’m hoping with these new experiences compounded by my previous educational
                 and professional background, I’ll leverage myself into a new position working full stack development
                 in the tech industry. As for who I am apart from my professional goals, I love to read, either non-fiction
